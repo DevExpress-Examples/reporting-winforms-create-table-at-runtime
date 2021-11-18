780b7b9d5a6f75a01c0d1fa5c8e2d023eb169b52
DevExpressExampleBot <fakeEmail>
2021-11-19 02:10:57 +0300

    Source auto update [skip ci]

--- VB/XtraReport1.vb
+++ VB/XtraReport1.vb
@@ -1,53 +1,52 @@
-Imports Microsoft.VisualBasic
 Imports System.Drawing
 Imports System.Drawing.Printing
 Imports DevExpress.XtraReports.UI
+
 ' ...
-
 Namespace XRTable_RuntimeCreation
-	Partial Public Class XtraReport1
-		Inherits XtraReport
 
-		#Region "#Report Creation"
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class XtraReport1
+        Inherits XtraReport
 
-		Private Sub XtraReport1_BeforePrint(ByVal sender As Object, ByVal e As PrintEventArgs) Handles MyBase.BeforePrint
-			Me.Detail.Controls.Add(CreateXRTable())
-		End Sub
-		#End Region
+'#Region "#Report Creation"
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Public Function CreateXRTable() As XRTable
-			Dim cellsInRow As Integer = 3
-			Dim rowsCount As Integer = 3
-			Dim rowHeight As Single = 25f
+        Private Sub XtraReport1_BeforePrint(ByVal sender As Object, ByVal e As PrintEventArgs)
+            Detail.Controls.Add(CreateXRTable())
+        End Sub
 
-			Dim table As New XRTable()
-			table.Borders = DevExpress.XtraPrinting.BorderSide.All
-			table.BeginInit()
+'#End Region
+        Public Function CreateXRTable() As XRTable
+            Dim cellsInRow As Integer = 3
+            Dim rowsCount As Integer = 3
+            Dim rowHeight As Single = 25F
+            Dim table As XRTable = New XRTable()
+            table.Borders = DevExpress.XtraPrinting.BorderSide.All
+            table.BeginInit()
+            For i As Integer = 0 To rowsCount - 1
+                Dim row As XRTableRow = New XRTableRow()
+                row.HeightF = rowHeight
+                For j As Integer = 0 To cellsInRow - 1
+                    Dim cell As XRTableCell = New XRTableCell()
+                    row.Cells.Add(cell)
+                Next
 
-			For i As Integer = 0 To rowsCount - 1
-				Dim row As New XRTableRow()
-				row.HeightF = rowHeight
-				For j As Integer = 0 To cellsInRow - 1
-					Dim cell As New XRTableCell()
-					row.Cells.Add(cell)
-				Next j
-				table.Rows.Add(row)
-			Next i
+                table.Rows.Add(row)
+            Next
 
-			AddHandler table.BeforePrint, AddressOf table_BeforePrint
-			table.AdjustSize()
-			table.EndInit()
-			Return table
-		End Function
+            AddHandler table.BeforePrint, New PrintEventHandler(AddressOf table_BeforePrint)
+            table.AdjustSize()
+            table.EndInit()
+            Return table
+        End Function
 
-		' The following code makes the table span to the entire page width.
-		Private Sub table_BeforePrint(ByVal sender As Object, ByVal e As PrintEventArgs)
-			Dim table As XRTable = (CType(sender, XRTable))
-			table.LocationF = New DevExpress.Utils.PointFloat(0F, 0F)
-			table.WidthF = Me.PageWidth - Me.Margins.Left - Me.Margins.Right
-		End Sub
-	End Class
+        ' The following code makes the table span to the entire page width.
+        Private Sub table_BeforePrint(ByVal sender As Object, ByVal e As PrintEventArgs)
+            Dim table As XRTable = CType(sender, XRTable)
+            table.LocationF = New DevExpress.Utils.PointFloat(0F, 0F)
+            table.WidthF = PageWidth - Margins.Left - Margins.Right
+        End Sub
+    End Class
 End Namespace
